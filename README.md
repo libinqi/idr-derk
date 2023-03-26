660c0144c3f197b0ca2a04fd0870688f8b3aadc4
libinqi <libinqi@outlook.com>
2023-03-26 22:56:37 +0800

    提交德科阅读器模块代码

---
+++ README.md
@@ -0,0 +1,5 @@
+##德科iDR身份证阅读器Node.js模块
+# 1、readCard,手动读取身份证信息
+# 2、openRealReadCard,打开异步后台读取身份证信息
+# 3、startRealReadCard,开始异步后台读取身份证信息
+# 4、stopRealReadCard,停止异步后台读取身份证信息
---
+++ index.js
@@ -0,0 +1,6 @@
+var iDR = require('./lib/DerkRead');
+
+module.exports.readCard=iDR.readCard;
+module.exports.openRealReadCard=iDR.openRealReadCard;
+module.exports.startRealReadCard=iDR.startRealReadCard;
+module.exports.stopRealReadCard=iDR.stopRealReadCard;
